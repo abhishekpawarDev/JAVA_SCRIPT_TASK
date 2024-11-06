a3e12ec2b7e6b2fe9debf6486a0dd7d9341a7de3
Abhishek Pawar <abhipawarraipur@gmail.com>
2024-11-06 12:55:39 +0530

    Create 1to10pattern.js

---
+++ 1to10pattern.js
@@ -0,0 +1,9 @@
+let num =1;
+for(let i = 0; i <=3;i++){
+    let patteren = '';
+    for(let j =0; j<=i;j++){
+        patteren = patteren + num;
+        num++;
+    }
+    console.log(patteren)
+}
