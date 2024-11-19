cabeb67ba7bf15fe365325f01a715aba538d7863
Abhishek Pawar <abhipawarraipur@gmail.com>
2024-11-19 19:45:22 +0530

    forLoop

---
+++ forIn&forOfLoop.js
@@ -0,0 +1,14 @@
+//ForIn loop
+
+let arr = [10, 20, 30, 40];
+for (let num of arr) {
+    console.log(`number is ${num}`);
+}
+
+// ForOf Loop
+
+for (let num of arr) {
+    console.log(`number is ${num}`);
+}
+
+
